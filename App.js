c2b3bb5f103c9b333526373573f494fe9644272f
Sharma572 <raunakk572@gmail.com>
2022-07-20 14:31:01 +0530

    added hamburger

--- App.js
+++ App.js
@@ -1,3 +1,25 @@
-console.log("Worked");
-let dom = document.getElementsByClassName("DOM").innerHtml ;
-console.log(dom);
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+
+
+  //  form validate
+  function validate(){
+    let name = document.querySelector("Name").value;
+    if(name == null || name == ""){
+      alert("Enter Your Name");
+    }
+    
+    
+      
+    }
+    
+    
+  
